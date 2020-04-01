089d3076a5f83ce498a1b6f52bc936f66b15b3c2
Conner Jowitt <conner.jowitt@infinityworks.com>
2020-04-01 14:42:14 +0100

    update readme, moved validation into a seperate function

--- README.md
+++ README.md
@@ -11,29 +11,7 @@ npm install
 npm run start
 ```
 
- * Use the examples from the OAS (`components/examples/`) sym-linking them into the app.
-
-## Deployment
-
-Redeploy the API Proxy. See the main [README.md](../README.md).
-
 ## Endpoints
 
-- [ ] GET    `/Patient`
-- [x] GET    `/Patient/{nhs_number}`
-- [ ] PATCH  `/Patient/{nhs_number}`
-- [ ] GET    `/Patient/{nhs_number}/RelatedPerson`
-- [ ] POST   `/Patient/{nhs_number}/RelatedPerson`
-- [ ] GET    `/Patient/{nhs_number}/RelatedPerson/{id}`
-- [ ] PUT    `/Patient/{nhs_number}/RelatedPerson/{id}`
-- [ ] DELETE `/Patient/{nhs_number}/RelatedPerson/{id}`
-- [ ] GET    `/Patient/{nhs_number}/ReasonableAdjustment`
-- [ ] POST   `/Patient/{nhs_number}/ReasonableAdjustment`
-- [ ] GET    `/Patient/{nhs_number}/ReasonableAdjustment/{id}`
-- [ ] PUT    `/Patient/{nhs_number}/ReasonableAdjustment/{id}`
-- [ ] DELETE `/Patient/{nhs_number}/ReasonableAdjustment/{id}`
-- [ ] GET    `/Patient/{nhs_number}/Address`
-- [ ] POST   `/Patient/{nhs_number}/Address`
-- [ ] GET    `/Patient/{nhs_number}/Address/{id}`
-- [ ] PUT    `/Patient/{nhs_number}/Address/{id}`
-- [ ] DELETE `/Patient/{nhs_number}/Address/{id}`
+- [x] GET    `/Prescription/`
+- [ ] POST   `/Prescription/translate`
--- routes/patient/post-prescription.js
+++ routes/patient/post-prescription.js
@@ -3,13 +3,49 @@ const mocks = require('../../services/mocks')
 const objectHelper = require("../../helpers/object-helper")
 const requestValidator = require("../../validators/request-validator")
 
+function verifyBundleEntryContents(request) {
+    if (!requestValidator.verifyBundleEntryContainsMedicationRequest(request)) {
+        throw Boom.badRequest(
+            "Request must contain at least one 'MedicationRequest' entry",
+            {operationOutcomeCode: "value", apiErrorCode: "MISSING_FIELD"}
+        )
+    }
+
+    if (!requestValidator.verifyBundleEntryContainsOnePatient(request)) {
+        throw Boom.badRequest(
+            "Request must contain one 'Patient' entry",
+            {operationOutcomeCode: "value", apiErrorCode: "MISSING_FIELD"}
+        )
+    }
+
+    if (!requestValidator.verifyBundleEntryContainsPractitioner(request)) {
+        throw Boom.badRequest(
+            "Request must contain at least one 'Practitioner' entry",
+            {operationOutcomeCode: "value", apiErrorCode: "MISSING_FIELD"}
+        )
+    }
+
+    if (!requestValidator.verifyBundleEntryContainsOneEncounter(request)) {
+        throw Boom.badRequest(
+            "Request must contain one 'Encounter' entry",
+            {operationOutcomeCode: "value", apiErrorCode: "MISSING_FIELD"}
+        )
+    }
+
+    if (!requestValidator.verifyBundleEntryContainsTwoOrganizations(request)) {
+        throw Boom.badRequest(
+            "Request must contain two 'Organization' entries",
+            {operationOutcomeCode: "value", apiErrorCode: "MISSING_FIELD"}
+        )
+    }
+}
+
 module.exports = [
 
     {
         method: 'POST',
         path: '/Prescription/translate',
         handler: (request, h) => {
-
             if (!requestValidator.verifyResourceTypeIsBundle(request)) {
                 throw Boom.badRequest(
                     "ResourceType must be 'Bundle' on request",
@@ -24,40 +60,7 @@ module.exports = [
                 )
             }
 
-            if (!requestValidator.verifyBundleEntryContainsMedicationRequest(request)) {
-                throw Boom.badRequest(
-                    "Request must contain at least one 'MedicationRequest' entry",
-                    {operationOutcomeCode: "value", apiErrorCode: "MISSING_FIELD"}
-                )
-            }
-
-            if (!requestValidator.verifyBundleEntryContainsOnePatient(request)) {
-                throw Boom.badRequest(
-                    "Request must contain one 'Patient' entry",
-                    {operationOutcomeCode: "value", apiErrorCode: "MISSING_FIELD"}
-                )
-            }
-
-            if (!requestValidator.verifyBundleEntryContainsPractitioner(request)) {
-                throw Boom.badRequest(
-                    "Request must contain at least one 'Practitioner' entry",
-                    {operationOutcomeCode: "value", apiErrorCode: "MISSING_FIELD"}
-                )
-            }
-
-            if (!requestValidator.verifyBundleEntryContainsOneEncounter(request)) {
-                throw Boom.badRequest(
-                    "Request must contain one 'Encounter' entry",
-                    {operationOutcomeCode: "value", apiErrorCode: "MISSING_FIELD"}
-                )
-            }
-
-            if (!requestValidator.verifyBundleEntryContainsTwoOrganizations(request)) {
-                throw Boom.badRequest(
-                    "Request must contain two 'Organization' entries",
-                    {operationOutcomeCode: "value", apiErrorCode: "MISSING_FIELD"}
-                )
-            }
+            verifyBundleEntryContents(request)
 
             return h.response(mocks.actualResponse).header('test', 'yo')
         }
